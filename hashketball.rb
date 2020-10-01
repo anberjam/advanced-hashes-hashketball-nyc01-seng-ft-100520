3cfad1245731eefe1d07249cd5e1b014fe32d90c
Anber Jamshed <anberjam@yahoo.com>
2020-10-01 22:28:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -220,6 +220,8 @@ end
 def player_stats(player)
   game = game_hash
   game.each do |location, t_c_p|
-    t_c_p.each do |
+    t_c_p.each do |key, value|
+      if key==:players
+        
   end
 end
